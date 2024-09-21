f134bcada652cf41ebb79a84edd14434baf1cb90
io <koto.games.xo@gmail.com>
2024-09-21 19:48:44 +0500

    feat(nvim): bufferline telescope

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,35 +1,39 @@
-local vim = vim
-local map = vim.keymap.set
-local set = vim.opt
-local Plug = vim.fn['plug#']
+local vim                     = vim
+local map                     = vim.keymap.set
+local set                     = vim.opt
+local Plug                    = vim.fn['plug#']
 
-set.ignorecase = true
-set.smartcase = true
-set.hlsearch = true
-set.incsearch = true
-set.swapfile = false
+set.ignorecase                = true
+set.smartcase                 = true
+set.hlsearch                  = true
+set.incsearch                 = true
+set.swapfile                  = false
 
-set.tabstop = 4
-set.softtabstop = 4
-set.shiftwidth = 4
-set.colorcolumn = "88" --8
+set.tabstop                   = 4
+set.softtabstop               = 4
+set.shiftwidth                = 4
+-- TODO: set.colorcolumn = "88" --8
 
-set.expandtab = true
+set.scrolloff                 = 8
+set.expandtab                 = true
+set.smarttab                  = true
+set.autoindent                = true
+set.smartindent               = true
 
-set.smarttab = true
-set.autoindent = true
-set.smartindent = true
-
-set.number = true
-set.relativenumber = true
-
+set.number                    = true
+set.relativenumber            = true
+vim.g.mapleader               = ","
 vim.g.airline_powerline_fonts = 1
+vim.g.netrw_banner            = 0
+vim.g.netrw_liststyle         = 3
+vim.g.netrw_browse_split      = 3
+
 vim.call('plug#begin')
 
 Plug('ryanoasis/vim-devicons')
 Plug('vim-airline/vim-airline')
 Plug('vim-airline/vim-airline-themes')
-Plug('nvim-treesitter/nvim-treesitter')
+Plug('nvim-treesitter/nvim-treesitter') --'luukvbaal/nnn.nvim')
 Plug('powerline/powerline')
 Plug('preservim/nerdcommenter')
 Plug('scrooloose/nerdtree')
@@ -39,6 +43,10 @@ Plug('tpope/vim-commentary')
 Plug('folke/which-key.nvim')
 Plug('lewis6991/gitsigns.nvim')
 Plug('xiyaowong/transparent.nvim')
+Plug('nvim-lua/plenary.nvim')
+Plug('nvim-telescope/telescope.nvim')
+Plug('nvim-tree/nvim-web-devicons')
+Plug('akinsho/bufferline.nvim')
 
 Plug('neovim/nvim-lspconfig')
 Plug('hrsh7th/nvim-cmp')
@@ -60,11 +68,16 @@ map('', '<C-n>', ':NERDTreeToggle<CR>', { noremap = true, silent = true })
 map('n', 'zs', ':q!<CR>')
 map('n', 'zo', ':wq!<CR>')
 
-map('i', '<C-B>', 'BUG: ')
-map('i', '<C-T>', 'TODO: ')
-map('i', '<C-W>', 'WARN: ')
-map('i', '<C-P>', 'PASSED: ')
+map('i', '<C-B>', ' BUG:')
+map('i', '<C-T>', ' TODO:')
+map('i', '<C-W>', ' WARN:')
+map('i', '<C-P>', ' PASSED:')
 
+local builtin = require('telescope.builtin')
+map('n', '<leader>ff', builtin.find_files, { desc = 'Telescope find files' })
+map('n', '<leader>fg', builtin.live_grep, { desc = 'Telescope live grep' })
+map('n', '<leader>fb', builtin.buffers, { desc = 'Telescope buffers' })
+map('n', '<leader>fh', builtin.help_tags, { desc = 'Telescope help tags' })
 
 -- BUG: V----- лох
 require("todo-comments").setup({
@@ -152,7 +165,7 @@ require('gitsigns').setup {
     },
     auto_attach                  = true,
     attach_to_untracked          = false,
-    current_line_blame           = false, -- Toggle with `:Gitsigns toggle_current_line_blame`
+    current_line_blame           = false,
     current_line_blame_opts      = {
         virt_text = true,
         virt_text_pos = 'eol', -- 'eol' | 'overlay' | 'right_align'
@@ -164,8 +177,8 @@ require('gitsigns').setup {
     current_line_blame_formatter = '<author>, <author_time:%R> - <summary>',
     sign_priority                = 6,
     update_debounce              = 100,
-    status_formatter             = nil,   -- Use default
-    max_file_length              = 40000, -- Disable if file is longer than this (in lines)
+    status_formatter             = nil, -- Use default
+    max_file_length              = 40000,
     preview_config               = {
         -- Options passed to nvim_open_win
         border = 'single',
@@ -176,4 +189,20 @@ require('gitsigns').setup {
     },
 }
 
+vim.opt.termguicolors = true
+require("bufferline").setup {
+    options = {
+        modified_icon = '󰄴',
+        close_icon = '',
+    },
+}
+map('n', 'gb', '<CMD>BufferLinePick<CR>')
+map('n', '<leader>ts', '<CMD>BufferLinePickClose<CR>')
+map('n', '<S-l>', '<CMD>BufferLineCycleNext<CR>')
+map('n', '<S-h>', '<CMD>BufferLineCyclePrev<CR>')
+map('n', ']b', '<CMD>BufferLineMoveNext<CR>')
+map('n', '[b', '<CMD>BufferLineMovePrev<CR>')
+map('n', 'gs', '<CMD>BufferLineSortByDirectory<CR>')
+
 require("lsp")
+require("nnn").setup()
